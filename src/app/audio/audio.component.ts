9cd43881f40f7dd10292c8cd1b85f28ccf6f8288
Güttinger Luca <luca.guettinger@cmiag.ch>
2021-04-13 14:00:10 +0200

    canvas

--- src/app/audio/audio.component.ts
+++ src/app/audio/audio.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-audio',
   templateUrl: './audio.component.html',
   styleUrls: ['./audio.component.css']
 })
-export class AudioComponent implements OnInit {
+export class AudioComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('myCanvas') myDiv: ElementRef<HTMLCanvasElement> | undefined;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+
+    const ctx = this.myDiv?.nativeElement.getContext('2d');
+    if (ctx == null) {
+      console.log();
+      return;
+    }
+
+    ctx.font = '30px Arial';
+    ctx.strokeText('Hello World', 10, 50);
+  }
 }
